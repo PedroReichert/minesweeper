c842b8b257f21c31038fecd2118115e40409d64f
Pedro Reichert <pedroreichert@gmail.com>
2021-12-04 11:05:07 -0300

    create migrations

---
+++ database/migrations/2021_12_04_135919_create_fields_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFieldsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('fields', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id');
+            $table->bigInteger('columns');
+            $table->bigInteger('row');
+            $table->boolean('is_completed')->default(0);
+            $table->boolean('is_won')->default(0);
+            $table->timestamps();
+            $table->softDeletesTz();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('fields');
+    }
+}
